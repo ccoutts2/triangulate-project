63a1558cc8ee14967b4969e9f40b1545ec004446
Chris Coutts <chris.dcoutts@gmail.com>
2023-12-26 18:13:45 +0000

    Add basic styling to mobile/tablet/desktop

---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="nav">
+        <ul className="nav__list">
+          <div className="nav__left-side">
+            <li className="nav__item">
+              <a className="nav__link" href={"#"}>
+                TRIANGULATE
+              </a>
+            </li>
+          </div>
+          <div className="nav__right-side">
+            <li className="nav__item">
+              <a className="nav__link" href={"#"}>
+                ABOUT
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href={"#"}>
+                MEET
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href={"#"}>
+                LOGIN
+              </a>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
