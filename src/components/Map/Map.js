a85368dc0656c32de2aa5a921e1cb4c704df08c4
Chris Coutts <chris.dcoutts@gmail.com>
2024-01-15 16:24:57 +0000

    Default custom markers

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,36 +3,8 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import Friends from "../Friends/Friends";
-import SelectedFriend from "../SelectedFriend/SelectedFriend";
 
 const Map = () => {
-  // Setting Friends State
-
-  const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
-
-  const [friends, setFriends] = useState(null);
-  const [selectedFriend, setSelectedFriend] = useState(false);
-
-  const handleFriendClick = (friend) => {
-    setSelectedFriend(friend);
-  };
-
-  const fetchFriends = async () => {
-    try {
-      const response = await axios.get(`${baseURL}/meet`);
-      setFriends(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchFriends();
-  }, []);
-
-  // Setting Map State
-
   const mapboxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
   mapboxgl.accessToken = mapboxAccessToken;
@@ -44,20 +16,46 @@ const Map = () => {
   const [zoom, setZoom] = useState(10.8);
 
   useEffect(() => {
-    if (map.current) return;
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [lng, lat],
-      zoom: zoom,
-    });
+    const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${baseURL}/meet`);
+        const pubData = response.data;
 
-    if (!map.current) return;
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
+        pubData.forEach((pub) => {
+          const { id, name, address } = pub;
+          const { latitude, longitude } = address;
+
+          const marker = new mapboxgl.Marker()
+            .setLngLat([longitude, latitude])
+            .addTo(map.current);
+
+          const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h3>${name}</h3>`
+          );
+          marker.setPopup(popup);
+        });
+      } catch (error) {
+        console.log("Error");
+      }
+    };
+
+    fetchData();
+  }, [lng, lat, zoom]);
+
+  if (map.current) return;
+  map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: [lng, lat],
+    zoom: zoom,
+  });
+
+  if (!map.current) return;
+  map.current.on("move", () => {
+    setLng(map.current.getCenter().lng.toFixed(4));
+    setLat(map.current.getCenter().lat.toFixed(4));
+    setZoom(map.current.getZoom().toFixed(2));
   });
 
   //   const mapContainer = useRef(null);
@@ -101,8 +99,6 @@ const Map = () => {
 
   return (
     <section>
-      <Friends friends={friends} handleFriendClick={handleFriendClick} />
-      <SelectedFriend selectedFriend={selectedFriend} />
       <div className="filler"></div>
       <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
--- src/pages/Meet/Meet.js
+++ src/pages/Meet/Meet.js
@@ -1,9 +1,37 @@
 // import "./Meet.scss";
 import Map from "../../components/Map/Map";
+import Friends from "../../components/Friends/Friends";
+import SelectedFriend from "../../components/SelectedFriend/SelectedFriend";
+
+import axios from "axios";
+import { useRef, useEffect, useState } from "react";
 
 const Meet = () => {
+  const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
+
+  const [friends, setFriends] = useState(null);
+  const [selectedFriend, setSelectedFriend] = useState(false);
+
+  const handleFriendClick = (friend) => {
+    setSelectedFriend(friend);
+  };
+
+  const fetchFriends = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/meet`);
+      setFriends(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFriends();
+  }, []);
   return (
     <main>
+      <Friends friends={friends} handleFriendClick={handleFriendClick} />
+      <SelectedFriend selectedFriend={selectedFriend} />
       <Map />
     </main>
   );
