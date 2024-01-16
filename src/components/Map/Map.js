259fed34dab981ba3f5d64bc64944696290bb4ac
Chris Coutts <chris.dcoutts@gmail.com>
2024-01-16 20:51:35 +0000

    Refactor code to implement more useable react npm package

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,74 +2,111 @@ import "./map.scss";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useRef, useEffect, useState } from "react";
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import "mapbox-gl/dist/mapbox-gl.css";
+import Map, { Marker, Popup } from "react-map-gl";
+// import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+// import "mapbox-gl/dist/mapbox-gl.css";
 
 const Map = () => {
-  // Setting Map State
+  //   // Setting Map State
 
-  const mapboxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+  //   const mapboxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-  mapboxgl.accessToken = mapboxAccessToken;
+  //   mapboxgl.accessToken = mapboxAccessToken;
 
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-0.0815);
-  const [lat, setLat] = useState(51.54);
-  const [zoom, setZoom] = useState(10.8);
+  //   const mapContainer = useRef(null);
+  //   const map = useRef(null);
+  //   const [lng, setLng] = useState(-0.0815);
+  //   const [lat, setLat] = useState(51.54);
+  //   const [zoom, setZoom] = useState(10.8);
 
-  useEffect(() => {
-    if (map.current) return;
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/dark-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
+  //   const bounds = [
+  //     [-0.3275, 51.4108], // Southwest coordinates
+  //     [0.0374, 51.6103], // Northeast coordinates
+  //   ];
 
-    if (!map.current) return;
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
+  //   useEffect(() => {
+  //     if (map.current) return;
+  //     map.current = new mapboxgl.Map({
+  //       container: mapContainer.current,
+  //       style: "mapbox://styles/mapbox/dark-v11",
+  //       center: [lng, lat],
+  //       zoom: zoom,
+  //       maxBounds: bounds,
+  //     });
 
-  useEffect(() => {
-    const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
+  //     if (!map.current) return;
+  //     map.current.on("move", () => {
+  //       setLng(map.current.getCenter().lng.toFixed(4));
+  //       setLat(map.current.getCenter().lat.toFixed(4));
+  //       setZoom(map.current.getZoom().toFixed(2));
+  //     });
+  //   });
 
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${baseURL}/maps`);
-        const pubData = response.data;
+  // useEffect(() => {
+  //   const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
 
-        pubData.forEach((pub) => {
-          console.log(pub);
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get(`${baseURL}/maps`);
+  //       const pubData = response.data;
 
-          const marker = new mapboxgl.Marker()
-            .setLngLat([pub.address.longitude, pub.address.latitude])
-            .addTo(map.current);
+  //       pubData.forEach((pub) => {
+  //         console.log(pub);
 
-          const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-            `<h4>${pub.pub}</h4>`
-          );
-          marker.setPopup(popup);
-        });
-      } catch (error) {
-        console.log(error); //
-      }
-    };
+  //         const marker = new mapboxgl.Marker()
+  //           .setLngLat([pub.address.longitude, pub.address.latitude])
+  //           .addTo(map.current);
 
-    fetchData();
-  }, [map, lng, lat, zoom]);
+  //         const popup = new mapboxgl.Popup({ offset: 25 })
+  //           .addClassName("custom-marker")
+  //           .setHTML(`<h4>${pub.pub}</h4>`);
+  //         marker.setPopup(popup);
+  //       });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, [map, lng, lat, zoom]);
+
+  //   useEffect(() => {
+  //     const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
+
+  //     const fetchData = async () => {
+  //       try {
+  //         const response = await axios.get(`${baseURL}/friends`);
+  //         const friendData = response.data;
+
+  //         friendData.forEach((friend) => {
+  //           console.log(friend);
+
+  //           const marker = new mapboxgl.Marker({
+  //             color: "F3180A",
+  //           })
+  //             .setLngLat([friend.homeAddress.longitude, friend.homeAddress.latitude])
+  //             .addTo(map.current);
+
+  //           // const popup = new mapboxgl.Popup({
+  //           //   offset: 25,
+  //           // }).setHTML(`<h4>${friend.name}</h4>`);
+  //           // marker.setPopup(popup);
+  //         });
+  //       } catch (error) {
+  //         console.log(error); //
+  //       }
+  //     };
+
+  //     fetchData();
+  //   }, [map, lng, lat, zoom]);
 
   return (
     <section>
-      <div className="filler"></div>
-      <div className="sidebar">
+      <div></div>
+      {/* <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-      <div ref={mapContainer} className="map-container"></div>
+      <div ref={mapContainer} className="map-container"></div> */}
     </section>
   );
 };
--- src/pages/Meet/Meet.js
+++ src/pages/Meet/Meet.js
@@ -1,10 +1,13 @@
 import "./meet.scss";
-import Map from "../../components/Map/Map";
+// import Map from "../../components/Map/Map";
 import Friends from "../../components/Friends/Friends";
 import SelectedFriend from "../../components/SelectedFriend/SelectedFriend";
+import Map, { Marker, Popup } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Pins from "../../components/Pins/Pins";
 
 import axios from "axios";
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useMemo } from "react";
 
 const Meet = () => {
   const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
@@ -29,13 +32,67 @@ const Meet = () => {
   useEffect(() => {
     fetchFriends();
   }, []);
+
+  const [pubData, setPubData] = useState([]);
+
+  useEffect(() => {
+    const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${baseURL}/maps`);
+        setPubData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const friendMarkers = useMemo(
+    () =>
+      friends
+        ? friends.map((friend) => (
+            <Marker
+              longitude={friend.homeAddress.longitude}
+              latitude={friend.homeAddress.latitude}>
+              <Pins />
+            </Marker>
+          ))
+        : [],
+    [friends]
+  );
+
+  const pins = useMemo(
+    () =>
+      pubData.map((pub) => (
+        <Marker longitude={pub.address.longitude} latitude={pub.address.latitude}>
+          <Pins />
+        </Marker>
+      )),
+    [pubData]
+  );
+
   return (
     <main>
       <div className="friends-wrapper">
         <Friends friends={friends} handleFriendClick={handleFriendClick} />
         <SelectedFriend selectedFriend={selectedFriend} />
       </div>
-      <Map />
+      <div className="map-wrapper">
+        <Map
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          initialViewState={{
+            longitude: -0.0815,
+            latitude: 51.54,
+            zoom: 10.8,
+          }}
+          mapStyle="mapbox://styles/mapbox/dark-v11">
+          {pins}
+          {friendMarkers}
+        </Map>
+      </div>
     </main>
   );
 };
