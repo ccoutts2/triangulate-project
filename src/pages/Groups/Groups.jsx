0340e8278990a08007b06de8b888bdc6543609f5
Chris Coutts <chris.dcoutts@gmail.com>
2024-02-08 08:59:53 +0000

    WIP - set up groups page

--- src/pages/Groups/Groups.jsx
+++ src/pages/Groups/Groups.jsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import "./groups.scss";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import hero from "../../assets/images/hero.svg";
 
 const Groups = () => {
+  //   const { groupdId } = useParams();
+
   const baseURL = process.env.REACT_APP_FRIENDS_API_URL;
   const [groups, setGroups] = useState(null);
 
@@ -25,13 +29,22 @@ const Groups = () => {
 
   return (
     <main className="groups-page">
-      {groups.map((group) => {
-        return (
-          <article key={group.id}>
-            <h3>{group.group_name}</h3>
-          </article>
-        );
-      })}
+      <section className="groups-page__product-cards">
+        {groups.map((group) => {
+          return (
+            <Link to={`/meet/${group.id}/pubs`}>
+              <article key={group.id} className="groups-page__product-card">
+                <img
+                  className="groups-page__image"
+                  src={hero}
+                  alt="arty picture of people sitting at a bar"
+                />
+                <h3 className="groups-page__title">{group.group_name}</h3>
+              </article>
+            </Link>
+          );
+        })}
+      </section>
     </main>
   );
 };
